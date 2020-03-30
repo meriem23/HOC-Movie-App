59a84e6a98de379cc9f497dfc28cffa6ec2e28b2
meriem23 <esra.msallem@gmail.com>
2020-03-30 14:49:02 +0100

    Updates

---
+++ precache-manifest.8f89da9bc3c472ba7e1c841ba51156a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f69cbaca5c9af7277ae528cfdeb64095",
+    "url": "/HOC-Movie-App/index.html"
+  },
+  {
+    "revision": "0fee0f01867cad21aa43",
+    "url": "/HOC-Movie-App/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "ecf6f11207d2ff0c9b1d",
+    "url": "/HOC-Movie-App/static/css/main.6581f263.chunk.css"
+  },
+  {
+    "revision": "0fee0f01867cad21aa43",
+    "url": "/HOC-Movie-App/static/js/2.047d7319.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/HOC-Movie-App/static/js/2.047d7319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf6f11207d2ff0c9b1d",
+    "url": "/HOC-Movie-App/static/js/main.47e7834b.chunk.js"
+  },
+  {
+    "revision": "94ce0f3a3292496e7e87",
+    "url": "/HOC-Movie-App/static/js/runtime-main.d4340fd3.js"
+  }
+]);
